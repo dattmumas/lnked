3fc5f2b653c8ddaaedeb6c4aa140d906c787936e
matthewdumas <mttdumas@gmail.com>
2025-05-20 13:38:13 -0700

    removed type override files

--- src/types/react.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
- 
-declare module "react" {
-  export type ElementType = any;
-  export const useState: any;
-  export const useEffect: any;
-  const content: any;
-  export = content;
-}
