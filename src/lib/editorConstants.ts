bad5478bbc205bc6fff11d12013608134db6d208
dattmumas <143631978+dattmumas@users.noreply.github.com>
2025-05-23 20:27:45 -0700

    refactor: remove unused editor code

---
+++ src/lib/editorConstants.ts
@@ -0,0 +1,20 @@
+export const EMPTY_LEXICAL_STATE = JSON.stringify({
+  root: {
+    children: [
+      {
+        type: 'paragraph',
+        children: [],
+        direction: null,
+        format: '',
+        indent: 0,
+        version: 1,
+      },
+    ],
+    direction: null,
+    format: '',
+    indent: 0,
+    type: 'root',
+    version: 1,
+  },
+});
+
